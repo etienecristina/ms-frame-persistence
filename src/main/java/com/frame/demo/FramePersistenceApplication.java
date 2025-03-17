54a4ed1b0bba335d16e110a7163ed3c0d522b604
etienecristina <etiene.scristina@gmail.com>
2025-03-17 14:15:29 -0300

    Iniciando o projeto

---
+++ src/main/java/com/frame/demo/FramePersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.frame.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FramePersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FramePersistenceApplication.class, args);
+	}
+
+}
